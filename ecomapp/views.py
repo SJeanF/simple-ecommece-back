70de8b76bcac9df288fba40c927ec2ab46fdf444
SJeanF <fonsecasantosjean@gmail.com>
2026-03-08 00:31:50 -0300

    feat: adicionar view e url para get de um produto com base no id

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
 # from .products import products
+
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -11,8 +14,19 @@ def getRoutes(request):
   return Response('Hello Jenan')
 
 @api_view(['GET'])
-def getProducts(resquest):
+def getProducts(request):
   products = Product.objects.all()
   serialized = ProductSerializer(products, many=True)
 
+  print(type(serialized.data))
+
   return Response(serialized.data)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+
+  product = get_object_or_404(Product, _id=pk)
+  serialized = ProductSerializer(product, many=False)
+
+  return Response(serialized.data)
+
