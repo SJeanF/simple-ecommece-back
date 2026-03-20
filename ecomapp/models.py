083cac37b74002a5e9f468b0f09e3df8f177f041
SJeanF <fonsecasantosjean@gmail.com>
2026-03-20 12:19:53 -0300

    feat: adicionar model de Order e de OrderItem e executar migrações

--- ecomapp/models.py
+++ ecomapp/models.py
@@ -18,5 +18,24 @@ class Product(models.Model):
 
   def __str__(self):
     return self.name
-  
-  
+
+
+class Order(models.Model):
+  _id = models.AutoField(primary_key=True, editable=False)
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  is_completed = models.BooleanField(default=False)
+
+  def __str__(self):
+    return f'order {self._id} of {self.user}'
+
+
+class OrderItem(models.Model):
+  _id = models.AutoField(primary_key=True, editable=False)
+  product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+  order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, related_name='items')
+  quantity = models.PositiveIntegerField(default=0, null=True, blank=True)
+  date_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+
+  def __str__(self):
+    return self.product.name
